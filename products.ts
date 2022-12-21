d5046e03b1f00e1af9c08250be0cd31339d03686
Monakhov-Maksym <71643346+Monakhov-Maksym@users.noreply.github.com>
2022-12-21 18:39:05 +0200

    Update products.ts

---
+++ products.ts
@@ -0,0 +1,20 @@
+export interface ProductI {
+"tovarid": string
+"id": string
+"name": string
+"parent": string
+"type": string
+"category_id": string
+"tosale": string
+"workshop_id": string
+"tax_id": string,
+"price_in": string,
+"price": string,
+"uktzed": string,
+"barcodes": [],
+"markup": string,
+"foodcost": string
+}
+//https://online.skyservice.pro/ru/api/products/full/?ot=desc&of=tovarid&search=&categories=[]&sklad=1&pagelimit=9999
+
+
