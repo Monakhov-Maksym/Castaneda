db054904af47b3a88a75a4429e5d00cc1ea79c5e
Monakhov-Maksym <71643346+Monakhov-Maksym@users.noreply.github.com>
2022-12-21 18:38:20 +0200

    Update clients.ts

---
+++ clients.ts
@@ -0,0 +1,9 @@
+export interface ClientsI {
+    "clientEmail": string,
+        "clientId": string,
+        "clientName": string,
+        "clientPhone": string,
+        "clientBalance": string,
+        "loyaltyGroup": string
+}
+//https://online.skyservice.pro/ru/api3/clients/?ot=asc&of=counterparty_id&search=&pagelimit=9999
